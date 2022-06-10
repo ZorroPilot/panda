3afc91ceaec97195433befed4a6b26ab3cb71ae0
Shane Smiskol <shane@smiskol.com>
2022-06-09 23:22:05 -0700

    Bosch should never need to send 0x1fa

--- tests/safety/test_honda.py
+++ tests/safety/test_honda.py
@@ -513,7 +513,7 @@ class TestHondaBoschLongSafety(HondaButtonEnableBase, TestHondaBoschSafetyBase):
   MAX_BRAKE = -3.5
 
   STEER_BUS = 1
-  TX_MSGS = [[0xE4, 1], [0x1DF, 1], [0x1EF, 1], [0x1FA, 1], [0x30C, 1], [0x33D, 1], [0x33DA, 1], [0x33DB, 1], [0x39F, 1], [0x18DAB0F1, 1]]
+  TX_MSGS = [[0xE4, 1], [0x1DF, 1], [0x1EF, 1], [0x30C, 1], [0x33D, 1], [0x33DA, 1], [0x33DB, 1], [0x39F, 1], [0x18DAB0F1, 1]]
   FWD_BLACKLISTED_ADDRS = {2: [0xE4, 0xE5, 0x33D, 0x33DA, 0x33DB]}
 
   def setUp(self):
